3d07ba8562a44e6e28b1523bfe7086677f0e79fb
thasmo <thasmo@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-06-14 21:43:23 +0000

    Cleanup.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/hype_directory/trunk@34311 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Provides a basic system for storing contact data.',
 	'category' => 'fe',
 	'shy' => 0,
-	'version' => '0.0.1',
+	'version' => '0.1.1',
 	'dependencies' => 'extbase,fluid',
 	'conflicts' => '',
 	'priority' => '',
@@ -23,7 +23,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'module' => '',
 	'state' => 'alpha',
 	'uploadfolder' => 0,
-	'createDirs' => 'uploads/hype/directory/contact/images',
+	'createDirs' => 'uploads/hype/directory/contact/images/',
 	'modify_tables' => '',
 	'clearcacheonload' => 0,
 	'lockType' => '',
@@ -44,7 +44,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:13:{s:12:"ext_icon.gif";s:4:"7300";s:17:"ext_localconf.php";s:4:"a224";s:14:"ext_tables.php";s:4:"b897";s:14:"ext_tables.sql";s:4:"ee85";s:46:"Configuration/FlexForms/directory.flexform.xml";s:4:"a5ec";s:25:"Configuration/TCA/tca.php";s:4:"098f";s:40:"Configuration/TCA/Icons/contact.icon.png";s:4:"1518";s:38:"Configuration/TCA/Icons/group.icon.png";s:4:"3afb";s:37:"Configuration/TCA/Icons/role.icon.png";s:4:"4fb4";s:38:"Configuration/TypoScript/constants.txt";s:4:"40dd";s:34:"Configuration/TypoScript/setup.txt";s:4:"afb9";s:40:"Resources/Private/Language/locallang.xml";s:4:"e9a0";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"eb39";}',
+	'_md5_values_when_last_written' => '',
 	'suggests' => array(
 	),
 );
